6d3c19d0c131534acb36ab0736fa4eb6e4751fea
Mario Mejia <mariomejiamartinez08@gmail.com>
2025-05-09 21:54:57 -0600

    Camviamos el mensaje de app.get y el comentario de la constante PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'); //Importamos express ala aplicacion app.js
 const app = express(); // marcamos el punto de partida de la aplicacion
 
-const PORT= process.env.PORT || 3000; //establecemos el puerto de ecucha en el 3000
+const PORT= process.env.PORT || 3000; //establecemos la varible de entrono que escuche en el puerto, de lo contrario usara 3000
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto 3000");
@@ -9,6 +9,6 @@ app.listen(PORT, () => {
 
  //Primer ruta
 app.get("/", (req, res) => {
-    res.send("Hola mundo"); //Mostramos el mensaje al conectarnos a la ruta
+    res.send(`Aplicacion corriendo en el puerto ${PORT}`); //Mostramos el mensaje al conectarnos a la ruta
 })
 
