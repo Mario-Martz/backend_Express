b7d2e14769584226c214f44c1197f72b7eb396a5
Mario Mejia <mariomejiamartinez08@gmail.com>
2025-05-09 21:47:30 -0600

    Se usaran varables de entorno

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express'); //Importamos express ala aplicacion app.js
 const app = express(); // marcamos el punto de partida de la aplicacion
-const port = 3000; //establecemos el puerto de ecucha en el 3000
 
-app.listen(port, () => {
+const PORT= process.env.PORT || 3000; //establecemos el puerto de ecucha en el 3000
+
+app.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto 3000");
 })
 
